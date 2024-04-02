bb91e6aaced9397f2e8b36e5e953811c38d577ea
Stefan Tomik <stefantomic18@yahoo.com>
2024-04-02 22:46:05 +0200

    domasna

---
+++ Homework3/Homework3/PrintInConsole.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework3
+{
+    public class PrintInConsole
+    {
+        public static void Print<T>(T item)
+        {
+            Console.WriteLine(item);
+        }
+
+
+        public static void PrintCollection<T>(List<T> items)
+        {
+            foreach (var item in items)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ Homework3/Homework3/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Homework3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            PrintInConsole.Print("Hello, world!");
+            PrintInConsole.Print(555);
+
+            List<int> numbers = new List<int> { 1, 2, 3, 4, 5 };
+            PrintInConsole.PrintCollection(numbers);
+
+            List<string> names = new List<string> { "Stefan", "Mitko", "Goran" };
+            PrintInConsole.PrintCollection<string>(names);
+        }
+    }
+}
